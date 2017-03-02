c61d3a1ccff84f55d77b3d87e053ad607ef26ce5
iposton <ianposton@sbcglobal.net>
2017-03-02 13:37:19 -0800

    heroku won't update push changes

--- client/app/database.js
+++ client/app/database.js
@@ -1,17 +1,3 @@
-import firebase from 'firebase';
 
-const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOM,
-    databaseURL: process.env.DB_URL,
-    storageBucket: process.env.STRG_BKT,
-    messagingSenderId: process.env.MS_ID
-};
-
-firebase.initializeApp(config);
-const database = firebase.database();
-
-
-export default database;
 
   
