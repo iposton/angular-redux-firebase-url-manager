47411926b5b9a81478554ba0b625db54f49d603f
iposton <ianposton@sbcglobal.net>
2017-03-02 13:49:31 -0800

    heroku restart

--- client/app/database.js
+++ client/app/database.js
@@ -1,3 +1,17 @@
+import firebase from 'firebase';
 
+const config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOM,
+    databaseURL: process.env.DB_URL,
+    storageBucket: process.env.STRG_BKT,
+    messagingSenderId: process.env.MS_ID
+};
+
+firebase.initializeApp(config);
+const database = firebase.database();
+
+
+export default database;
 
   
